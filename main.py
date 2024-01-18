c02bae7681c86eaf9ada2efbdd6dbecb45d5f09a
NIRMAL Sandeep Singh <sandeep.nirmal@iutseine.u-paris.domain>
2024-01-18 12:21:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ num = 67
 #num = int(input("Enter a number: "))
 # define a flag variable
 # Sandeep-Singh
-flag = True
+flag = False
 if num == 1:
 print(num, "is not a prime number")
 elif num > 1:
@@ -12,7 +12,7 @@ elif num > 1:
 for i in range(2, num):
 if (num % i) == 0:
 # if factor is found, set flag to True
-flag = False
+flag = True
 # break out of loop
 break
 # check if flag is True
